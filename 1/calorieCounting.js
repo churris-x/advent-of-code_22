2a1a5b4cef9db2308425b60247e21f46580b211b
Francisco Altimari <francisco@altimari.nl>
2022-12-15 15:10:36 +0100

    Solve part 1

--- 1/calorieCounting.js
+++ 1/calorieCounting.js
@@ -1,5 +1,37 @@
 const elves = require('./input.json');
 const totalInventory = JSON.parse(JSON.stringify(elves.totalInventory));
 
+const egInventory = `1000
+2000
+3000
 
-console.log(totalInventory);
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+const countCal = elf => elf.reduce((sum, food) => Number(food) + sum ,0);
+
+const findHighestCalorie = inventory => inventory
+	.split('\r\n\r\n')
+	.map(elf => countCal(elf.split('\r\n')))
+	.reduce((highest, current) => highest > current ? highest : current, 0);
+
+
+ console.log(findHighestCalorie(totalInventory));
+
+
+
+/* Part 1
+Wrong guesses:
+	12446004
+
+Correct:
+	69795
+*/
