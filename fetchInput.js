2498009ce7741cfb93dd1e7cf704c756553d1ca8
Francisco Altimari <francisco@altimari.nl>
2022-12-19 16:05:39 +0100

    Fetch with function or by calling the file

--- fetchInput.js
+++ fetchInput.js
@@ -1,16 +1,44 @@
 require('dotenv').config()
+const fs = require('fs');
 
-const token = process.env.AUTH_TOKEN;
-const url = `https://adventofcode.com/2022/day/${day}/input`;
+const { AUTH_TOKEN } = process.env;
 
-const getDayInput = async day => {
+const getInputByDay = async day => {
+	if (!day) throw `Function getInputByDay needs a day, got instead: ${day}`;
+	if (day < 0) throw 'Function getInputByDay: day needs to be a positive number';
+
+	const url = `https://adventofcode.com/2022/day/${day}/input`;
 	const response = await fetch(url, {
 		'headers': {
-			'cookie': `session=${token}`,
+			'cookie': `session=${AUTH_TOKEN}`,
 		}
 	});
 	const data = await response.text();
 	return data;
 };
 
-// getDayInput(1);
+
+// Fetches and saves input to a file when this file is called
+const inputDay = process.argv[2];
+
+if (inputDay && !isNaN(inputDay)) {
+	(async () => {
+		const input = await getInputByDay(inputDay);
+		
+		fs.writeFile(`${inputDay}/input.txt`, input, error => { 
+			if (error) throw error;
+			console.log(`Input for day ${inputDay} wrote successfuly.`);
+		});
+	})();
+}
+
+// const date = new Date();
+// const currentDay = {
+// 	day: date.getDate(),
+// 	month: date.getMonth() + 1,
+// 	year: date.getFullYear(),
+// }
+
+module.exports = {
+	getInputByDay,
+};
