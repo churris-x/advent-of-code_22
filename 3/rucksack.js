4d55044a3fedd083c5f9dbee8b33cc770c172e62
Francisco Altimari <francisco@altimari.nl>
2022-12-22 17:10:44 -0500

    Solve day 3 part 2

--- 3/rucksack.js
+++ 3/rucksack.js
@@ -4,26 +4,49 @@ const input = fs.readFileSync('./input.txt').toString().slice(0, -1);
 
 const priority = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-const findCommonItem = (a, b) => [...a]
-	.reduce((common, current) => b.includes(current) ? current : common);
+const findCommonItems = (a, b) => [...a]
+	.reduce((common, current) => b.includes(current) ? [...common, current] : common, []);
 
-const findHighestPriority = sacks => sacks
+const findTotalPriority = sacks => sacks
 	.split('\n')
 	.map( sack => {
 		const part = sack.length / 2;
-		const commonItem = findCommonItem(sack.slice(part), sack.slice(0, part));
+		const commonItem = findCommonItems(sack.slice(part), sack.slice(0, part))[0];
 		return priority.indexOf(commonItem);
 	})
 	.reduce((sum, item) => sum + item, 0);
 
 
-console.log(findHighestPriority(eg));
-console.log(findHighestPriority(input));
+console.log(findTotalPriority(eg));
+console.log(findTotalPriority(input));
 
+// ----------------------------------------------------------------------------
+
+const findBadge = (a, b, c) => findCommonItems(
+	findCommonItems(a, b).join(''),
+	findCommonItems(b, c).join('')
+)[0];
+
+const findTotalBadgePriority = groups => groups
+	.split('\n')
+	.reduce((sum, sack) => {
+		const [total, group] = sum;
+		
+		if (group.length === 2) {
+			
+			const groupPriority = priority.indexOf(findBadge(...group, sack));
+			return [total + groupPriority, []];
+		}
+
+		return [total, [...group, sack]]
+	}, [0, []]);
+
+console.log(findTotalBadgePriority(eg)[0]);
+console.log(findTotalBadgePriority(input)[0]);
 /*
 Wrong guesses:
 	
-
 Correct:
 	1) 7848
+	2) 2616
 */
