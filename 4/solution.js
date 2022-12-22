62d412422521c14189cd91090f06ffb1bc8d0fe3
Francisco Altimari <francisco@altimari.nl>
2022-12-22 18:42:09 -0500

    Solve day 4 part 1

--- 4/solution.js
+++ 4/solution.js
@@ -2,17 +2,27 @@ const fs = require('fs');
 const eg = fs.readFileSync('./eg.txt').toString().slice(0, -1);
 const input = fs.readFileSync('./input.txt').toString().slice(0, -1);
 
+const bContainsA = ([lowerA, upperA], [lowerB, upperB]) => (lowerA >= lowerB && upperA <= upperB);
 
+const totalContains = pairs => pairs
+	.split('\n')
+	.map(pair => pair.split(',')
+		.map(range => range.split('-')
+			.map(i => Number(i))))
+	.reduce((sum, [a, b]) => bContainsA(a, b) || bContainsA(b, a) ? sum + 1 : sum, 0);
 
-
-
-console.log(placeholder(eg));
-console.log(placeholder(input));
+console.log(totalContains(eg));
+console.log(totalContains(input));
 
 /*
 Wrong guesses:
-	
+	663 too high
+	660 too high
+	330 too low
+	427
+	// fucking stupid javascript type coersion fuck off you duck type cunt
+
 Correct:
-	1)
-	2)
+	1) 651
+	2) 
 */
