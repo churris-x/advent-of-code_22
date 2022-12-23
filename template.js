8e1f8ee86696bcaa00eefac814dcbe270a19d42d
Francisco Altimari <francisco@altimari.nl>
2022-12-23 12:08:41 -0500

    Add solution template

---
+++ template.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const eg = fs.readFileSync('./eg.txt').toString().slice(0, -1);
+const input = fs.readFileSync('./input.txt').toString().slice(0, -1);
+
+// Part 1 ---------------------------------------------------------------------
+
+// console.log(placeholder(eg));
+// console.log(placeholder(input));
+
+// Part 2 ---------------------------------------------------------------------
+
+// console.log(placeholder(eg));
+// console.log(placeholder(input));
+
+/*
+Wrong guesses:
+
+Correct:
+	1) 
+	2) 
+*/
