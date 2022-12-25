b644f09188582f2ba2db1f044e2df5fc80a8e7fc
Francisco Altimari <francisco@altimari.nl>
2022-12-25 10:13:21 -0500

    Day 5 progress

--- 5/solution.js
+++ 5/solution.js
@@ -2,10 +2,32 @@ const fs = require('fs');
 const eg = fs.readFileSync('./eg.txt').toString().slice(0, -1);
 const input = fs.readFileSync('./input.txt').toString().slice(0, -1);
 
+/*
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+*/
+
 // const [crates, moves] = input.split('\n\n');
 const [crates, moves] = eg.split('\n\n');
 
+const processInput = crates => crates
+	.split('\n')
+	.map(i=> [...`${i} `]
+		.reduce(([output, sum, count], current) => {
+			if (count + 1 === 4) return [[...output, sum], '', 0];
+			return [output, `${sum}${current}`, count + 1];
+		}, [[], '', 0])[0]
+	)
+
+	
+
 console.log(crates);
+console.log(processInput(crates));
+
+
 
 // console.log(placeholder(eg));
 // console.log(placeholder(input));
