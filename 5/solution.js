11aabe81c937b0e806852e37ccb1df447b414421
Francisco Altimari <francisco@altimari.nl>
2022-12-26 10:44:37 -0500

    Day 5 progress

--- 5/solution.js
+++ 5/solution.js
@@ -2,36 +2,27 @@ const fs = require('fs');
 const eg = fs.readFileSync('./eg.txt').toString().slice(0, -1);
 const input = fs.readFileSync('./input.txt').toString().slice(0, -1);
 
-/*
-    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-
-*/
-
 // const [crates, moves] = input.split('\n\n');
 const [crates, moves] = eg.split('\n\n');
 
 const processInput = crates => crates
 	.split('\n')
 	.map(i=> [...`${i} `]
-		.reduce(([output, sum, count], current) => {
-			if (count + 1 === 4) return [[...output, sum], '', 0];
-			return [output, `${sum}${current}`, count + 1];
-		}, [[], '', 0])[0]
-	)
-
+		.reduce(([row, crate, count], current) => {
+			if (count + 1 === 4) return [[...row, crate], '', 0];
+			return [row, `${crate}${current}`, count + 1];
+		}, [[], '', 0])[0])
 	
+	.reduce((columns, item, index, array) => array[0][index]
+		? [...columns, array.map(row => row[index]).reverse()] 
+		: columns
+	, [])
 
 console.log(crates);
 console.log(processInput(crates));
 
-
-
 // console.log(placeholder(eg));
 // console.log(placeholder(input));
-
 
 // ----------------------------------------------------------------------------
 
