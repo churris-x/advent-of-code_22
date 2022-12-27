63eb6e178505af52a63a2b833027cedcd1182612
Francisco Altimari <francisco@altimari.nl>
2022-12-27 17:12:11 -0500

    Solve day 5 part 1

--- 5/solution.js
+++ 5/solution.js
@@ -2,9 +2,6 @@ const fs = require('fs');
 const eg = fs.readFileSync('./eg.txt').toString().slice(0, -1);
 const input = fs.readFileSync('./input.txt').toString().slice(0, -1);
 
-// const [crates, moves] = input.split('\n\n');
-const [crates, moves] = eg.split('\n\n');
-
 const getColumns = crates => crates
 	.split('\n')
 	.map(i=> [...`${i} `]
@@ -19,16 +16,33 @@ const getColumns = crates => crates
 	, [])
 	.map(column => column.filter( crate => !!crate.trim()))
 
-const egColumns = getColumns(crates);
-
-const moveCrates = (columns, moveset) => {
-
+const moveCrates = (columns, ammount, from, to) => {
+	for (let i = 0; i < ammount; i++) {	
+		const crate = columns[from -1].pop()
+		columns[to -1].push(crate);
+	}
 };
 
-// console.log(crates);
-console.log(egColumns);
-console.log(moves);
-console.log(moveCrates(eg));
+const organizeCrates = (columns, moveset) => {
+	const array = JSON.parse(JSON.stringify(columns));
+
+	moveset
+		.split('\n')
+		.forEach(item => {
+			const move = item.split(' ');
+			const [ammount, from, to] = [move[1], move[3], move[5]];
+			moveCrates(array, ammount, from, to);
+		});
+
+	return array;
+};
+
+const getTopCrates = columns => columns.reduce((sum, column) => sum + column.slice(-1)[0][1], '');
+
+const [crates, moves] = input.split('\n\n');
+// const [crates, moves] = eg.split('\n\n');
+
+console.log(getTopCrates(organizeCrates(getColumns(crates), moves)));
 
 // ----------------------------------------------------------------------------
 
