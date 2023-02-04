9dfc9f4baf8d2e59daa9c4a9b4549c31e4d8d107
Francisco Altimari <francisco@altimari.nl>
2023-02-04 23:47:34 +0100

    Day 6 progress

--- 6/solution.js
+++ 6/solution.js
@@ -3,10 +3,13 @@ const eg = fs.readFileSync(require.resolve('./eg.txt')).toString().slice(0, -1);
 const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0, -1);
 
 /*
-bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
-nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6
-nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
-zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
+Buffer                         | Marker | data stream begins after 
+-------------------------------------------------------------------------------
+mjqjpqmgbljsphdztnvjfqwrcgsmlb:    jpqm   character 7 
+bvwbjplbgvbhsrlpgdmjqwftvncz:      vwbj   character 5
+nppdvjthqldpwncqszvftbrmjlhg:      pdvj   character 6
+nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: rfnt   character 10
+zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw:  zqfr   character 11
 */
 
 // Part 1 ---------------------------------------------------------------------
@@ -16,8 +19,12 @@ const getMarker = buffer => [...buffer].reduce((marker, item, index) => {
 	if (sum.length === 4 && [...sum].every(i => [...sum.matchAll(i)].length === 1)) return sum;
 	return `${marker}${item}`;
 }, '');
+console.log(getMarker(eg.split('\n')[0]));
 
 console.log(getMarker(eg.split('\n')[0]));
+console.log(getMarker(eg.split('\n')[1]));
+console.log(getMarker(eg.split('\n')[2]));
+console.log(getMarker(eg.split('\n')[3]));
 // console.log(getMarker(input));
 
 // Part 2 ---------------------------------------------------------------------
