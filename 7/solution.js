3d2d206ee11d725c87505eb4d9c6f152999d9bd0
Francisco Altimari <francisco@altimari.nl>
2023-02-07 11:08:38 +0100

    Day 7 progress

--- 7/solution.js
+++ 7/solution.js
@@ -9,6 +9,8 @@ ok how in the fuck am I even going to do this?
 Maybe a reduce that stores the current repository, 
 returning a [{'path': value}] dictionary
 
+Another way would be to couple the commands with their output
+
 $ cd /
 $ ls
 dir a 				/
@@ -17,24 +19,55 @@ dir a 				/
 dir d 				/
 $ cd a
 $ ls
-dir e 				/a/
-29116 f 			/a/
-2557 g 				/a/
-62596 h.lst 		/a/
+dir e 				/a
+29116 f 			/a
+2557 g 				/a
+62596 h.lst 		/a
 $ cd e
 $ ls
-584 i 				/a/e/
+584 i 				/a/e
 $ cd ..
 $ cd ..
 $ cd d
 $ ls
-4060174 j 			/d/
-8033020 d.log 		/d/
-5626152 d.ext 		/d/
-7214296 k 			/d/
+4060174 j 			/d
+8033020 d.log 		/d
+5626152 d.ext 		/d
+7214296 k 			/d
+
+{
+	path: '/',
+	tree: [{
+		'/a' : 0,
+		'/b.txt': 14848514,
+		'/c.dat': 8504156
+	}],
+}
+
+	cd .. => '/a/e'.slice(0, '/a/e'.lastIndexOf('/'))
+	cd f => '/a/e' => `${/a/e}/${f}`
+	cd / => '/'
+
 */
 
-// console.log('1) eg: ', placeholder(eg));
+const placeholder = commands => commands.split('\n').reduce((dir, command) => {
+	const {path, tree} = dir;
+	const args = command.split(' ');
+
+	if (command.includes('$ ls')) return dir;
+	
+	// if '$ cd' change path
+	if (command.includes('$ cd ')) {
+		if (args[2] === '/') return {path: '', tree}
+		if (args[2] === '..') return {path: path.slice(0, path.lastIndexOf('/')), tree}
+		return {path: `${path}/${args[2]}`, tree}
+	}
+	// if file add to tree
+	return {path, tree: {...tree, [`${path}/${args[1]}`]: Number(args[0]) || 0}}
+
+}, {path: '', tree: {'/': 0}}).tree;
+
+console.log('1) eg: ', placeholder(eg));
 // console.log('1) input: ', placeholder(input));
 
 // Part 2 ---------------------------------------------------------------------
