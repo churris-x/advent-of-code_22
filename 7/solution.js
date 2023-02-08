6ea0783ea52bd2bca1e6eb38e9f56fc6aa6eecaf
Francisco Altimari <francisco@altimari.nl>
2023-02-08 17:53:30 +0100

    Tidy up day 7 solution

--- 7/solution.js
+++ 7/solution.js
@@ -11,43 +11,11 @@ returning a [{'path': value}] dictionary
 
 Another way would be to couple the commands with their output
 
-$ cd /
-$ ls
-dir a 				/
-14848514 b.txt 		/
-8504156 c.dat 		/
-dir d 				/
-$ cd a
-$ ls
-dir e 				/a
-29116 f 			/a
-2557 g 				/a
-62596 h.lst 		/a
-$ cd e
-$ ls
-584 i 				/a/e
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j 			/d
-8033020 d.log 		/d
-5626152 d.ext 		/d
-7214296 k 			/d
-
-{
-	path: '/',
-	tree: [{
-		'/a' : 0,
-		'/b.txt': 14848514,
-		'/c.dat': 8504156
-	}],
-}
-
 	cd .. => '/a/e'.slice(0, '/a/e'.lastIndexOf('/'))
 	cd f => '/a/e' => `${/a/e}/${f}`
 	cd / => '/'
 
+Answer was both!
 */
 
 const getTree = commands => commands
@@ -78,13 +46,7 @@ const getFolderSizes = tree => tree
 
 const getSizeBelowLimit = (folders, limit = 100000) => folders
 	.filter(([path, size]) => size <= limit)
-	.reduce((sum, [path, size]) => sum + size, 0)
-
-const getSizeAboveLimit = (folders, update = 30000000) => folders
-	.filter(([path, size]) => 70000000 - folders[0][1] + size >= update)
-	.sort(([a, sizeA], [b, sizeB]) => sizeA - sizeB)
-	[0][1]
-	
+	.reduce((sum, [path, size]) => sum + size, 0)	
 
  console.log(getTree(eg), '\n\n', getFolderSizes(getTree(eg)), '\n\n');
 
@@ -92,6 +54,11 @@ console.log('1) eg: ', getSizeBelowLimit(getFolderSizes(getTree(eg))));
 console.log('1) input: ', getSizeBelowLimit(getFolderSizes(getTree(input))));
 
 // Part 2 ---------------------------------------------------------------------
+
+const getSizeAboveLimit = (folders, update = 30000000) => folders
+	.filter(([path, size]) => 70000000 - folders[0][1] + size >= update)
+	.sort(([a, sizeA], [b, sizeB]) => sizeA - sizeB)
+	[0][1]
 
 console.log('2) eg: ', getSizeAboveLimit(getFolderSizes(getTree(eg))));
 console.log('2) input: ', getSizeAboveLimit(getFolderSizes(getTree(input))));
@@ -101,5 +68,5 @@ Wrong guesses:
 
 Correct:
 	1) 1118405
-	2) 
+	2) 12545514
 */
