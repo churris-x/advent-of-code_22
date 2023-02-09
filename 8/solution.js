7865083bc82a57cc8197754e3b829039c2bda8e2
Francisco Altimari <francisco@altimari.nl>
2023-02-09 13:48:48 +0100

    Day 8 progress

--- 8/solution.js
+++ 8/solution.js
@@ -4,8 +4,39 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 
 // Part 1 ---------------------------------------------------------------------
 
-// console.log('1) eg: ', placeholder(eg));
-// console.log('1) input: ', placeholder(input));
+/*
+	hmm this is going to be difficult
+	N
+   W E
+	S
+
+	each direction will be a boolean, n: true means it is visible from the north nah
+
+	'0000'
+	'0001' n
+	'0010' e
+	'0100' s
+	'1000' w	
+
+*/
+
+const getRows = grid => grid.split('\n').map(row => row.split(''));
+const getColumns = grid => getRows(grid)
+	.map((row, rowIndex, array) => row
+		.map((i, index) => array[index][rowIndex])
+	);
+
+const placeholder = grid => {
+
+	return {
+		rows: getRows(grid),
+		columns: getColumns(grid),
+	}
+};
+
+console.log(eg);
+console.log('1) eg: ', placeholder(eg));
+ // console.log('1) input: ', placeholder(input));
 
 // Part 2 ---------------------------------------------------------------------
 
