fbb89abe72cd07a1643b60ec81d1a6dd7c50ef84
Francisco Altimari <francisco@altimari.nl>
2023-02-09 22:58:52 +0100

    Add day 8 part 2 todo and play with displaying

--- 8/solution.js
+++ 8/solution.js
@@ -6,11 +6,11 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 
 /*
 	hmm this is going to be difficult
-
-	N
-   W E
-	S
-	
+         
+	  N   
+	W ^ E 
+	  S  
+         
 	Store visibility as a sum of powers of 2, then check values with bitwise operator
 	eg: 
 		north + west = 9
@@ -25,7 +25,7 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 
 */
 
-const [u, n, e, s, w] = [0, 1, 2, 4, 8];
+const [north, east, south, west] = [1, 2, 4, 8];
 
 const getRows = grid => grid.split('\n').map(row => row.split(''));
 const getColumns = rows => rows
@@ -33,7 +33,7 @@ const getColumns = rows => rows
 		.map((i, index) => array[index][rowIndex])
 	);
 
-const checkVisible = (array, heading = 0) => array.reduce(({highest, set}, tree) => {
+const checkVisible = (array, heading) => array.reduce(({highest, set}, tree) => {
 	const [height, visible] = tree;
 	if (highest < height) return { highest: height, set: [...set, [height, visible + heading]]}
 
@@ -42,22 +42,42 @@ const checkVisible = (array, heading = 0) => array.reduce(({highest, set}, tree)
 }, {highest: -1, set: []}).set;
 
 const checkGrid = grid => {
-	const rows = getRows(grid).map(i => i.map(i => [i, u]));
+	const rows = getRows(grid).map(i => i.map(i => [i, 0]));
 
 	const checkedRows = rows
-		.map(i => checkVisible(i, w))
+		.map(i => checkVisible(i, west))
 		.map(i => [...i].reverse())
-		.map(i => checkVisible(i, e))
+		.map(i => checkVisible(i, east))
+		// .map(i => [...i].reverse())			// only needed if orientation is important
 
 	const checkedColumns = getColumns(checkedRows)
-		.map(i => checkVisible(i, n))
+		.map(i => checkVisible(i, north))
 		.map(i => [...i].reverse())
-		.map(i => checkVisible(i, s))
+		.map(i => checkVisible(i, south))
+		// .map(i => [...i].reverse())			// only needed if orientation is important
 
 	return checkedColumns
 };
 
-// const showVisibility = grid => 
+
+const symbol = [' ', '╵', '╶', '└', '╷', '│', '┌', '├', '╴', '┘', '─', '┴', '┐', '┤', '┬', '┼']
+const getHeadings = visible => {
+	let styled = '';
+	styled += visible & north ?  'N' : ' ';
+	styled += visible & east ?  'E' : ' ';
+	styled += visible & south ?  'S' : ' ';
+	styled += visible & west ?  'W' : ' ';
+	return styled;
+}
+
+ const showVisibility = grid => getColumns(grid)
+ 	.map(row => row
+ 		.map(([height, visible]) => 
+ 			getHeadings(visible)
+ 		)
+ 		.join('│')
+	)
+	.join('\n────┼────┼────┼────┼────\n')
 
 const getVisible = grid => grid
 	.reduce((count, rows) => count + rows
@@ -65,8 +85,9 @@ const getVisible = grid => grid
 		, 0)
 	, 0)
 
-console.log(eg);
-console.log('1) eg: ', checkGrid(eg));
+// console.log(eg);
+// console.log(getColumns(getColumns(getRows(eg))).map(row => row.map(i=> ` ${i} `).join('│')).join('\n───┼───┼───┼───┼───\n'));
+// console.log(showVisibility(checkGrid(eg)));
 console.log('1) eg: ', getVisible(checkGrid(eg)));
 console.log('1) input: ', getVisible(checkGrid(input)));
 
