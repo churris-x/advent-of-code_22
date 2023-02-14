117794836276c2697d04f0e87329efaa3af45f13
Francisco Altimari <francisco@altimari.nl>
2023-02-14 17:25:39 +0100

    Catch error and log it with file path

--- fetchInput.js
+++ fetchInput.js
@@ -2,14 +2,16 @@ require('dotenv').config()
 const fs = require('fs');
 
 const { AUTH_TOKEN } = process.env;
+const file = process.argv[1];
+
 
 // TODO(Fran): check if no day use Date.getDate();
-// TODO(Fran): add year param? new Date('1/1/21').getFullYear() to allow 2 digit
+// TODO(Fran): add year param? new Date(`1/1/${21}`).getFullYear() to allow 2 digit
 
-const getInputByDay = async (day) => {
-	if (!AUTH_TOKEN) throw 'Function getInputByDay is missing AUTH_TOKEN';
-	if (!day || isNaN(day)) throw `Function getInputByDay needs a day number, got instead: ${day}`;
-	if (day < 1 || day > 25) throw `Function getInputByDay: invalid day, needs to be between 1-25, got instead: ${day}`;
+const getInputByDay = async day => {
+	if (!AUTH_TOKEN) throw `${file} is missing AUTH_TOKEN`;
+	if (!day || isNaN(day)) throw `${file} needs a day number, got instead: ${day}`;
+	if (day < 1 || day > 25) throw `${file}: invalid day, needs to be between 1-25, got instead: ${day}`;
 
 	const url = `https://adventofcode.com/2022/day/${day}/input`;
 	const response = await fetch(url, {
@@ -27,13 +29,17 @@ const day = process.argv[2];
 
 if (day) {
 	(async () => {
-		const input = await getInputByDay(day);
-		
-		// TODO(Fran): check if folder already exists, if not create it
-		await fs.writeFile(`${day}/input.txt`, input, error => { 
-			if (error) throw error;
-			console.log(`Input for day ${day} wrote successfuly.`);
-		});
+		try {
+			const input = await getInputByDay(day);
+			
+			// TODO(Fran): check if folder already exists, if not create it
+			await fs.writeFile(`${day}/input.txt`, input, error => { 
+				if (error) throw error;
+				console.log(`Input for day ${day} wrote successfuly.`);
+			});
+		} catch (error) {
+			console.error(error)
+		}
 	})();
 }
 
