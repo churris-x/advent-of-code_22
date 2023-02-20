4701f77e4222f749d11597ef9259ec642ea90545
Francisco Altimari <francisco@altimari.nl>
2023-02-21 00:09:22 +0100

    Begin work on get file

---
+++ utils/getFile.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+// NOTE(Fran): this is still WIP
+const getFile = async path => {
+ 	
+ 	const fullPath = process.argv[1];
+ 	const pwd = fullPath.slice(0, fullPath.lastIndexOf('/'))
+
+ 	try {
+	 	const file = await fs.readFileSync(pwd?.concat(path.slice(1)));
+		return file.toString().slice(0, -1);
+	} catch (error) {
+		console.log(error);
+		console.log(`File "${path}" not found!`);
+		return '';
+	}
+}
+
+module.exports = getFile;
