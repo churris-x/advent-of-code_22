05c74bce6f8efd6627dc6ea10b8e5fa3051b3f6e
Francisco Altimari <francisco@altimari.nl>
2023-03-07 17:58:13 +0100

    Day 9 progress

--- 9/solution.js
+++ 9/solution.js
@@ -5,25 +5,60 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 // Part 1 ---------------------------------------------------------------------
 /*
 	This is way more complex than I thought...
-
+	Ah ok the tail warps on diagonals jesus, prefessor rework here we go
 	save position as []
 
-	┌────────┬────────┐
-	│head    │tail    │
-	├────────┼────────┤
-	│        │        │
-	│ 4┌──┐  │ 4 ..   │
-	│ 3│┼┴┼┐ │ 3  ..  │
-	│ 2└─▼├┘ │ 2..►.  │
-	│ 1   │  │ 1   .  │
-	│ 0───┘  │ 0...   │
-	│  01234 │  01234 │
-	│        │        │
-	└────────┴────────┘
-
+	┌─────────┬─────────┐
+	│head     │tail     │
+	├─────────┼─────────┤
+	│         │         │
+	│ 4 ┌──┐  │ 4  ##   │
+	│ 3 │┼┴┼┐ │ 3   ##  │
+	│ 2 └─▼├┘ │ 2 ##►#  │
+	│ 1    │  │ 1    #  │
+	│ 0 ───┘  │ 0 ###   │
+	│   01234 │   01234 │
+	│         │         │
+	└─────────┴─────────┘
 */
 
 const opposite = {U: 'D', R: 'L', D: 'U', L: 'R'};
+
+// const placeholder = moves => moves
+// 	.split('\n')
+// 	.map(item => item[0].repeat(item[2]))
+// 	.join('').split('')
+// 	.reduce(([distance, tiles], move, index, array) => {
+// 		const prevPosition = tiles.slice(-1)[0]
+// 		const prevMove = array[index -1] ?? '';
+// 		
+// 		let [x, y] = prevPosition
+// 			.split(';')
+// 			.map(i => i | 0);
+// 		switch (move) {
+// 			case 'U': y++; break;
+// 			case 'R': x++; break;
+// 			case 'D': y--; break;
+// 			case 'L': x--; break;
+// 		}
+// 		const position = `${x};${y}`
+// 
+// 
+// 		if (move.includes(prevMove)) {
+// 			obj = [1, [...tiles, position]];
+// 			console.log('same move', obj);
+// 			return obj
+// 		}
+// 		if (distance && move == opposite[prevMove]) {
+// 			obj = [0, tiles];
+// 			console.log('backwards', obj);
+// 			return obj
+// 		}
+// 		console.log('diagonal', [1, tiles]);
+// 		return [1, tiles];
+// 	}, [0, ['0;0']])[2]
+// 	.filter((tile, index, array) => array.indexOf(tile) === index)
+// 	.length
 
 const placeholder = moves => moves
 	.split('\n')
@@ -32,6 +67,7 @@ const placeholder = moves => moves
 	.reduce(([distance, tiles], move, index, array) => {
 		const prevPosition = tiles.slice(-1)[0]
 		const prevMove = array[index -1] ?? '';
+		
 		let [x, y] = prevPosition
 			.split(';')
 			.map(i => i | 0);
@@ -42,7 +78,6 @@ const placeholder = moves => moves
 			case 'D': y--; break;
 			case 'L': x--; break;
 		}
-
 		const position = `${x};${y}`
 
 
@@ -51,18 +86,28 @@ const placeholder = moves => moves
 			console.log('same move', obj);
 			return obj
 		}
-        if (distance && move == opposite[prevMove]) {
-        	obj = [0, tiles];
-        	console.log('backwards', obj);
-        	return obj
-        }
-        console.log('diagonal', [1, tiles]);
-		return [1, tiles];
-	}, [0, ['0;0']])[2]
-	   // .filter((tile, index, array) => array.indexOf(tile) === index)
-	   //  .length
+		if (distance && move == opposite[prevMove]) {
+			obj = [0, tiles];
+			console.log('backwards', obj);
+			return obj
+		}
 
-  // console.log('1) eg: ', placeholder('R 4'));
+		// still WIP
+		switch (prevMove) {
+			case 'U': y++; break;
+			case 'R': x++; break;
+			case 'D': y--; break;
+			case 'L': x--; break;
+		}
+		console.log('diagonal', [1, tiles]);
+		return [1, tiles];
+
+	}, [0, ['0;0']])[2]
+	// .filter((tile, index, array) => array.indexOf(tile) === index)
+	// .length
+
+
+  console.log('1) eg: ', placeholder('R 4'));
   console.log('1) eg: ', placeholder(eg));
  // console.log('1) input: ', placeholder(input));
 
