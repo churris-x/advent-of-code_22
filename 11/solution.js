cb6a9541bb33796c1b6df2a295a49515da0287dd
Francisco Altimari <francisco@altimari.nl>
2023-04-24 10:24:46 +0200

    Add day 11 puzzle

---
+++ 11/solution.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const eg = fs.readFileSync(require.resolve('./eg.txt')).toString().slice(0, -1);
+const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0, -1);
+
+// Part 1 ---------------------------------------------------------------------
+
+// console.log('1) eg: ', placeholder(eg));
+// console.log('1) input: ', placeholder(input));
+
+// Part 2 ---------------------------------------------------------------------
+
+// console.log('2) eg: ', placeholder(eg));
+// console.log('2) input: ', placeholder(input));
+
+/*
+Wrong guesses:
+
+Correct:
+    1) 
+    2) 
+*/
