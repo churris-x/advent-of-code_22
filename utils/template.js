69eb18eb9b62179d0602b933ea2c1539fedd5943
Francisco Altimari <francisco@altimari.nl>
2023-04-24 10:26:16 +0200

    Update template

--- utils/template.js
+++ utils/template.js
@@ -4,10 +4,14 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 
 // Part 1 ---------------------------------------------------------------------
 
+const placeholder = () => {};
+
 // console.log('1) eg: ', placeholder(eg));
 // console.log('1) input: ', placeholder(input));
 
 // Part 2 ---------------------------------------------------------------------
+
+// const placeholder = () => {};
 
 // console.log('2) eg: ', placeholder(eg));
 // console.log('2) input: ', placeholder(input));
