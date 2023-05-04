56da760028f0acccad9972664d9c7dcf96de2666
Francisco Altimari <francisco@altimari.nl>
2023-05-04 07:49:07 +0200

    Day 11 progress

--- 11/solution.js
+++ 11/solution.js
@@ -3,11 +3,31 @@ const eg = fs.readFileSync(require.resolve('./eg.txt')).toString().slice(0, -1);
 const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0, -1);
 
 // Part 1 ---------------------------------------------------------------------
+/*
+    A lot of string parsing this time
+    startingItems -> will be inspect in the order as well
+    new Function( 'string' ).call(args, of, func);
 
-// console.log('1) eg: ', placeholder(eg));
+
+    new Function(`(new, old) => ${line 2}`)
+    new Function('new', 'old', 'body')
+
+*/
+
+
+const getItems = state => state
+    .split('\n\n')
+    .map(item => item
+        .split('\n')[1]
+        .split(':')[1]
+    )
+
+console.log('1) eg: ', getItems(eg));
 // console.log('1) input: ', placeholder(input));
 
 // Part 2 ---------------------------------------------------------------------
+
+// const placeholder = () => {};
 
 // console.log('2) eg: ', placeholder(eg));
 // console.log('2) input: ', placeholder(input));
